8c2b580892ac081222d51ad623ebe866d63a54c3
Simon Young <simon.young83@me.com>
2020-09-05 14:23:03 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -15,8 +15,9 @@ function ageChecker(age) {if (age >=13 && age<=19) {
   {if (age >=20) { return "You are a grownup"}
 }}}
 
-function ternaryTeenager(age) {
-
+function ternaryTeenager(age) {if (age >=13 && <=19) {
+return "You are a teenager"}
+{return "You are not a teenager"}
 }
 
 function switchAge(age) {
