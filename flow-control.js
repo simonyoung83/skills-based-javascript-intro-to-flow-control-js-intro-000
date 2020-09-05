d5367154191e3a66946f74784ebf642dddb1c20a
Simon Young <simon.young83@me.com>
2020-09-05 13:55:16 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -6,7 +6,7 @@ function basicTeenager(age) {if (age >=13 && age <=19){
 function teenager(age) {if (age>=13 && age<=19) {
   return "You are a teenager!"
 }
-return "you are not a teenager"
+return "You are not a teenager"
 }
 
 function ageChecker(age) {
